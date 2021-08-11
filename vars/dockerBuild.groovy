4eeefc8ae654158e5098942d23d96f04710b41ad
sanchitraj5 <87302682+sanchitraj5@users.noreply.github.com>
2021-08-11 16:01:44 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,1 +1,58 @@
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String contName = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
+
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+		registry = "$registryin" 	
+		dockerTag = "${docTag}_$BUILD_NUMBER"
+		containerName = "${contName}"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+	}
+		
+	agent { label 'docker' }
+	
+	triggers {
+		pollSCM '* * * * *'
+	}
+
+	stages {
+		stage("POLL SCM"){
+			steps {
+				 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
+			}
+		}	
+					
+		stage('BUILD IMAGE') { 
+			 steps { 
+				 script { 
+					 dockerImage = docker.build('"$registry:$dockerTag"') 
+				 }
+			} 
+		}
+					
+		stage('PUSH HUB') { 
+			 steps { 
+				 script { 
+					 docker.withRegistry( '', "$registryCredential" ) { 
+						 dockerImage.push() 
+					}
+				}		
+			} 
+		}
+					
+		stage('DEPLOY IMAGE') {
+			steps {
+				script { 
+					 docker.withRegistry( '', registryCredential ) { 
+						 dockerImage.run('-it --name "$containerName-$dockerTag"') 
+					}
+				} 
+			}
+		}
+	}
+			  
+}
+}
 
